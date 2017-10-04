2782cc4828dacac47447829f44527274161d4128
Nick Young <nyou045@UoA.auckland.ac.nz>
2017-10-05 10:55:17 +1300

    custom track ms precision

--- stream.py
+++ stream.py
@@ -21,18 +21,20 @@ def broadcast(msg, port=15000):
     s.sendto(msg.encode("utf-8"), (ip, port))
   s.sendto(msg.encode("utf-8"), ('<broadcast>', port))
 
-def get_file():
+def status():
   t.write("status\n".encode("utf-8"))
   r = t.read_until(">".encode("utf-8")).decode("utf-8")
-  if "file" not in r:
-    return None
-  m = re.search(r'(file://.+?) \)', r)
-  p = urlparse(m.group(1))
-  return url2pathname(p.path)
+  file = re.search(r'(file://.+?) \)', r)
+  if file:
+    file = urlparse(file.group(1))
+    file = url2pathname(file.path)
+  state = re.search(r'state (\w+)', r).group(1)
+  return file, state
 
 def get_time():
   t.write("get_time\n".encode("utf-8"))
-  s = t.read_until(">".encode("utf-8")).decode("utf-8").split("\n")[0].strip()
+  resp = t.read_until(">".encode("utf-8")).decode("utf-8")
+  s = resp.split("\n")[0].strip()
   if not s:
     return 0
   s = int(s)
@@ -53,9 +55,19 @@ s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 last_file = None
 last_sub = ""
 
+last_time = None
+
+last_state = None
+ticks = 0
+interval = .1
+
 while True:
-  filename = get_file()
-  if not filename:
+  filename, state = status()
+  if state != last_state:
+    last_state = state
+    print(state)
+
+  if not filename or state != "playing":
     continue
   if filename != last_file:
     filename_wo_ext = os.path.splitext(filename)[0]
@@ -70,9 +82,15 @@ while True:
     last_file = filename
 
   pos = get_time()
+  if pos != last_time:
+    last_time = pos
+    ticks = 0
+  else:
+    ticks += 1
+    pos += ticks * interval
   for sub in subs:
     if pos >= sub.start.total_seconds() and pos <= sub.end.total_seconds() and sub.content != last_sub and sub.content != "":
       last_sub = sub.content
       print(sub.content)
       broadcast(sub.content)
-  time.sleep(.1)
+  time.sleep(interval)
